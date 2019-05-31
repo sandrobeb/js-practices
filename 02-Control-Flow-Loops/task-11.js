00c1760cb5a30f559b3403ee1fb501e4a093016d
Sandro <sandrobeb16@gmail.com>
2019-05-31 20:17:53 +0400

    feat(02-Control-Flow-Loops): Finish task 10,11

--- 02-Control-Flow-Loops/task-11.js
+++ 02-Control-Flow-Loops/task-11.js
@@ -2,10 +2,10 @@ let arr = [5, 9, 12, -2, 3, 1]
 
 for (i in arr) {
     for (i = 0; i < arr.length; i++) {
-        if (arr[i] < arr[i + 1]) {
+        if (arr[i] < arr[i - 1]) {
             narr = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = narr;
+            arr[i] = arr[i - 1];
+            arr[i - 1] = narr;
         }
     }
 }
